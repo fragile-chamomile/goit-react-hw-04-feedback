3815b2b8540bd8f420f278a28c7fe06f400345e9
in-love-with-anime <98983660+fragile-chamomile@users.noreply.github.com>
2022-02-21 16:07:43 +0200

    propTypes

--- src/components/Feedback/Notification/Notification.js
+++ src/components/Feedback/Notification/Notification.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NotificationAlert } from './Notification.styled';
 
 function Notification({ message }) {
   return <NotificationAlert>{message}</NotificationAlert>;
 }
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default Notification;
