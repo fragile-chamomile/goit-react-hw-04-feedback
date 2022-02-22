a2f294f3450e4298840a943ab9afa25b5efed7f7
in-love-with-anime <98983660+fragile-chamomile@users.noreply.github.com>
2022-02-22 17:26:20 +0200

    settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Feedback from '../components/Feedback/Feedback';
 
-class App extends Component {
-  render() {
-    return (
-      <div style={{ margin: '50px auto 0', width: '600px' }}>
-        <Feedback />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div style={{ margin: '50px auto 0', width: '600px' }}>
+      <Feedback />
+    </div>
+  );
 }
 
 export default App;
